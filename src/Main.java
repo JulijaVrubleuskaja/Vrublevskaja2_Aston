dcb93cdd7bb6087d47208c545b3b8f9c306be2ca
Julia <irina@MacBook-Air-Mihail.local>
2024-03-29 12:23:35 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        // task 1
+        System.out.print("a= ");
+        int a= scanner.nextInt();
+        System.out.print("b= ");
+        int b= scanner.nextInt();
+        if (a==b){
+            System.out.println("a=b");
+        } else {
+            if (a>b){
+                System.out.println("a>b");
+            } else {System.out.println("a<b");}
+        }
+        int sum= a+b;
+        int minus= a-b;
+        int multiply=a*b;
+        float decrease =a/b;
+        System.out.println("a+b=" +sum);
+        System.out.println("a-b="+ minus);
+        System.out.println("a*b="+ multiply);
+        System.out.println("a/b="+ decrease);
+        //task 2
+        System.out.print("Введите строку 1 ");
+        String str1= scanner.next();
+        System.out.print("Введите строку 2 ");
+        String str2= scanner.next();
+        if (str1.equals(str2)){
+            System.out.println("Строки идентичны");
+        }else {
+            System.out.println("Строки НЕидентичны");
+        }
+        //task 3
+        int [] arr ={1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Четные числа");
+        for (int i=0; i<9; i++){
+            if (arr[i]%2==0){System.out.println(arr[i]);}
         }
     }
 }
